c00585f9e1446d5cb21fe6ef4b77acf046a5a0eb
Freesma <roman28112002dziubak@gmail.com>
2022-05-29 12:08:03 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import { Route, Routes} from 'react-router-dom'
+import {
+    HomePage,
+    ZhurnalStudentPage,
+    HometaskStudentPage,
+    AbsenteeismStudentPage,
+    TimeTableStudentPage,
+    CreatePost, AdminPage
+} from './pages'
+import {Login, NavMenu} from './components'
+import './styles/css/App.css'
+import axios from "axios";
 
 function App() {
+
+    const api = axios.create({
+        baseURL: 'http://localhost:5000/'
+    })
+
+    const [User, setUser] = useState({})
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {User.isLoginned ?   <div><div className='nav'><NavMenu role={User.roles} setUser={setUser} /></div> <div className='content'>
+            <Routes>
+                <Route path='/' element={<HomePage api={api}/>} />
+                <Route path={'/login'} render={(props) => (
+                    <Login {...props} setUser={setUser} />
+                )}/>
+                <Route path='/CreatePost' element={<CreatePost />} />
+                <Route path='/ZhurnalStudentPage' element={<ZhurnalStudentPage user={User} />} />
+                <Route path='/HometaskStudentPage' element={<HometaskStudentPage />} />
+                <Route path='/AbsenteeismStudentPage' element={<AbsenteeismStudentPage />} />
+                <Route path='/TimeTableStudentPage' element={<TimeTableStudentPage />} />
+                <Route path='/AdminPage' element={<AdminPage user={User} api={api} />} />
+                <Route path={'*'} element={<h1 className='Error'>PAGE NOT FOUND</h1>} />
+            </Routes>
+        </div> </div> : <Login user={User} setUser={setUser} />}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import {Provider} from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const userDefaultState = {
+    id: null,
+    fname: 'Maya',
+    lname: null,
+    login: null,
+    pass: null,
+    mobile: null,
+    email: null,
+    banned: null,
+    isLoginned: false
+}
+
+
+
+const userReducer = (state = userDefaultState, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {...state, id: state.id = action.payload.id, fname: state.fname = action.payload.fname,
+                lname: state.lname = action.payload.lname, login: state.login = action.payload.login,
+                pass: state.pass = action.payload.pass, mobile: state.mobile = action.payload.mobile,
+                email: state.email = action.payload.email, banned: state.banned = action.payload.banned}
+        default:
+            return state
+    }
+}
+
+const store = createStore(userReducer)
+
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
